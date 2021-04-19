a60255c892a3451b4561b8d8c7c93ac775577c5a
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-04-19 10:36:03 +0200

    Added chracter join to faction implementation

--- src/main/kotlin/com/mg/kata/Faction.kt
+++ src/main/kotlin/com/mg/kata/Faction.kt
@@ -1,4 +1,12 @@
 package com.mg.kata
 
 class Faction {
+
+    private val members = mutableListOf<Character>()
+
+    fun join(character: Character) {
+        members.add(character)
+    }
+
+    fun getMembers() = members.toList()
 }
